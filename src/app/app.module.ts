815aa2c12c701bceebd22119e4b5e7f1dc17a392
loghann <m.loghann@gmail.com>
2022-09-26 17:16:03 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor (
+        public email: string,
+        public lastname: string,
+        public firstname: string,
+        public password: string
+    ) {}
+}
